be36d5564c854ad72d6bb9a76254dbeac7500a0b
Buddhi Ayesha <buddhiayesha.13@cse.mrt.ac.lk>
2017-05-20 23:45:47 +0530

    Building raw text file

--- components/findLaw/src/main/java/org/titans/fyp/webcrawler/corpusBuilder/CorpusBuilder.java
+++ components/findLaw/src/main/java/org/titans/fyp/webcrawler/corpusBuilder/CorpusBuilder.java
@@ -22,26 +22,38 @@ public class CorpusBuilder {
 
     private static Logger log = LoggerFactory.getLogger(CorpusBuilder.class);
     private static DBConnection dbconnection = null;
-    static BufferedWriter bw = null;
-    static FileWriter fw = null;
-    static String text = null;
+    static BufferedWriter bw_lt = null;
+    static FileWriter fw_lt = null;
+
+    static BufferedWriter bw_rt = null;
+    static FileWriter fw_rt = null;
 
     public void run(String txtFileName) {
 
         try {
-            fw = new FileWriter(System.getProperty("user.dir") + File.separator + txtFileName + ".txt");
-            bw = new BufferedWriter(fw);
+            fw_lt = new FileWriter(System.getProperty("user.dir") + File.separator + "LemmatizedText" + File.separator + txtFileName + ".txt");
+            bw_lt = new BufferedWriter(fw_lt);
+
+            fw_rt = new FileWriter(System.getProperty("user.dir") + File.separator + "RawText" + File.separator + txtFileName + ".txt");
+            bw_rt = new BufferedWriter(fw_rt);
+
             dbconnection = DBConnection.getDBConnection();
 
             getCasesContent();
             getCasesSummary();
             getFootNoteContent();
 
-            if (bw != null)
-                bw.close();
+            if (bw_lt != null)
+                bw_lt.close();
 
-            if (fw != null)
-                fw.close();
+            if (fw_lt != null)
+                fw_lt.close();
+
+            if (bw_rt != null)
+                bw_rt.close();
+
+            if (fw_rt != null)
+                fw_rt.close();
 
         } catch (ClassNotFoundException ex) {
             log.error(ex.getMessage());
@@ -62,6 +74,7 @@ public class CorpusBuilder {
                 try {
                     String content = rs.getString("content");
                     LemmatizeAndWriteToFile(content);
+                    rawTextWriteToFile(content);
                 } catch (SQLException ex) {
                     log.error(ex.getMessage());
                 }
@@ -82,6 +95,7 @@ public class CorpusBuilder {
                 try {
                     String content = rs.getString("summary");
                     LemmatizeAndWriteToFile(content);
+                    rawTextWriteToFile(content);
                 } catch (SQLException ex) {
                     log.error(ex.getMessage());
                 }
@@ -102,6 +116,7 @@ public class CorpusBuilder {
                 try {
                     String content = rs.getString("content");
                     LemmatizeAndWriteToFile(content);
+                    rawTextWriteToFile(content);
                 } catch (SQLException ex) {
                     log.error(ex.getMessage());
                 }
@@ -134,7 +149,35 @@ public class CorpusBuilder {
                 // this is the lemmatized version of the token
                 String word = token.get(CoreAnnotations.LemmaAnnotation.class);
                 if (String.valueOf(word).chars().allMatch(Character::isLetter)) {
-                    bw.write(String.valueOf(word) + " ");
+                    bw_lt.write(String.valueOf(word) + " ");
+                }
+            }
+        }
+    }
+
+    public static void rawTextWriteToFile(String text) throws IOException {
+
+        // creates a StanfordCoreNLP object, with POS tagging, lemmatization, NER, parsing
+        Properties props = new Properties();
+        props.setProperty("annotators", "tokenize, ssplit, pos");
+        StanfordCoreNLP pipeline = new StanfordCoreNLP(props);
+
+        // create an empty Annotation just with the given text
+        Annotation document = new Annotation(text);
+
+        // run all Annotators on this text
+        pipeline.annotate(document);
+
+        List<CoreMap> sentences = document.get(CoreAnnotations.SentencesAnnotation.class);
+
+        for (CoreMap sentence : sentences) {
+            // traversing the words in the current sentence
+            // a CoreLabel is a CoreMap with additional token-specific methods
+            for (CoreLabel token : sentence.get(CoreAnnotations.TokensAnnotation.class)) {
+                // this is the lemmatized version of the token
+                String word = token.get(CoreAnnotations.LemmaAnnotation.class);
+                if (String.valueOf(word).chars().allMatch(Character::isLetter)) {
+                    bw_rt.write(String.valueOf(word) + " ");
                 }
             }
         }
